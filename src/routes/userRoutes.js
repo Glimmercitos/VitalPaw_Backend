a59d7b3de658d328567815fffe1be7cf7350daa9
Ferdiaz524 <00351519@uca.edu.sv>
2025-06-15 00:29:07 -0600

    ✨: adding new userRoutes config

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, updateUserRole, getUser } = require('../controllers/userController');
+const verifyFirebaseToken = require('../middlewares/verifyFirebaseToken');
+const { register, login, changeUserRole, getUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.put('/role', updateUserRole);            
+router.put('/role/:id', verifyFirebaseToken, changeUserRole);
 router.get('/getUser', getUser);
 
 module.exports = router;
