e16ee6dfd8c6ee908e1d01012d3840052e4f323d
EmelyChavez <00097923@uca.edu.sv>
2025-06-17 22:18:39 -0600

    📝: updating user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    googleId: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    firebaseUid: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    avatar: { type: String },
+    name: { type: String, required: true },
+    phone: { type: String },
     role: { type: String, default: 'cliente', enum: ['cliente', 'veterinario', 'admin'] }, 
+     vitalCoins: { type: Number, default: 100 },
+    cart: [{
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quantity: Number
+    }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
